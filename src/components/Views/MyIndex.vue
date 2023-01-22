455eb06de372043bc078166e6d0d173666db0788
HenriWelsch <s4jowels@uni-trier.de>
2023-01-22 17:24:21 +0100

    .

--- src/components/Views/MyIndex.vue
+++ src/components/Views/MyIndex.vue
@@ -4,16 +4,7 @@
         <v-text-field label="Abfahrt" placeholder="Abfahrt" outlined></v-text-field>
         <v-text-field label="Ankunft" placeholder="Ankunft" outlined></v-text-field>
         <v-text-field label="Uhrzeit" placeholder="Uhrzeit" outlined></v-text-field>
-
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            block
-            v-bind="attrs"
-            v-on="on"
-          >
-            Von hier nach da
-          </v-btn>
-        </template>
+        
 
     </v-container>
 </template>
